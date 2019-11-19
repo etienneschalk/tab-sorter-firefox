a455dfb96a96a868821614a2451c6aba76a7cec5
etienneschalk <45271239+etienneschalk@users.noreply.github.com>
2019-11-19 14:13:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tab Sorter
+
+## Help
+- [Mozilla Docs - Your first WebExtension](https://developer.mozilla.org/en/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension)
+- [Mozilla Docs - Working with the Tabs API](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_the_Tabs_API)
---
+++ tab_sorter.js
@@ -0,0 +1,74 @@
+function getCurrentWindowTabs() {
+  return browser.tabs.query({
+    currentWindow: true
+  });
+}
+
+function listTabs() {
+  getCurrentWindowTabs().then((tabs) => {
+    let tabsList = document.getElementById('tabs-list');
+    let currentTabs = document.createDocumentFragment();
+    let limit = 5;
+    let counter = 0;
+
+    tabsList.textContent = '';
+
+    for (let tab of tabs) {
+      if (counter < limit) {
+        let tabLi = document.createElement('li');
+        let tabLink = document.createElement('a');
+        tabLink.textContent = tab.url || tab.title || tab.id;
+        tabLink.setAttribute('href', tab.id);
+        tabLink.classList.add('switch-tabs');
+        tabLi.appendChild(tabLink);
+        currentTabs.appendChild(tabLi);
+      }
+      counter++;
+    }
+    tabsList.appendChild(currentTabs);
+  });
+}
+
+function sortTabs() {
+  getCurrentWindowTabs().then((tabs) => {
+    let notPinnedTabs = tabs.filter((tab) => !tab.pinned); // Not taking in account pinned tabs
+    console.log(notPinnedTabs)
+    // notPinnedTabs.sort((tabA, tabB) => tabA.url - tabB.url);
+    notPinnedTabs.sort((tabA, tabB) => tabA.url.localeCompare(tabB.url))
+    let newIds = notPinnedTabs.map((tab) => tab.id); // Get an array of the tabs ids
+
+    function onMoved(tab) {
+      console.log("Moved :" + tab.url);
+    }
+
+    function onError(error) {
+      console.log("Error :" + error);
+    }
+
+    newIds.forEach((value, index) => {
+      let moving = browser.tabs.move(
+        value, {
+          index: index
+        }
+      );
+      moving.then(onMoved, onError);
+    });
+  });
+}
+
+function zip(a, b) {
+  if (a.length !== b.length) {
+    return null;
+  }
+  return a.map((e, i) => [e, b[i]]);
+}
+
+// Event listeners -------------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", listTabs);
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "sort-tabs") {
+    sortTabs();
+  }
+});
