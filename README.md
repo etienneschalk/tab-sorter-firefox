e0cb27470ef3f8873c2c43ad7258b42953296ec6
etienneschalk <45271239+etienneschalk@users.noreply.github.com>
2023-05-14 22:43:43 +0200

    Feature/persist user parameters (#1)
    * Persist user settings - untested
    
    * formatting
    
    * Cleaning
    
    * PR

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Tab Sorter
 
+## Changelog
+
+- 2023-05-14: Persist extension settings (checkboxes)
+
 ## Help
+
 - [Mozilla Docs - Your first WebExtension](https://developer.mozilla.org/en/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension)
 - [Mozilla Docs - Working with the Tabs API](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_the_Tabs_API)
 - [Mozilla Docs - Anatomy of a WebExtension](https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/Anatomy_of_a_WebExtension#Background_scripts)
 - [Mozilla Docs - Commands](https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/manifest.json/commands)
 - [Mozilla Docs - Communication with background scripts](https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/Content_scripts#Communication_avec_les_scripts_darri%C3%A8re-plan)
+- [Mozilla Docs - Implement a settings page](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Implement_a_settings_page)
+
 ## Todo
+
 - Less naive filtering:
   - Better analyze of the URLs (exclude 'www.')
   - Taking into account the title for same domains (eg sorting youtube videos by title and not by id for better human readability)
@@ -20,7 +28,7 @@
 - Extraction by icon and connection with OneTab to reduce all tabs from a same domain
 - Optimization:
   - Quick reverse if a first sort was just clicked
-   (like a cache ; while no tab is moved after a sorting operation, if we choose the same operation but we reverse, we just reverse the tab without sorting all of them once again)
+    (like a cache ; while no tab is moved after a sorting operation, if we choose the same operation but we reverse, we just reverse the tab without sorting all of them once again)
   - Quicker sort for MRU (comparison between numbers)
   - Put current group of tabs at the end / the beginning
   - Detect duplicate table
--- popup-tab-sorter.js
+++ popup-tab-sorter.js
@@ -1,3 +1,6 @@
+const SORT_TABS_REVERSE = "sort-tabs-reverse";
+const SORT_TABS_ALL_WINDOWS = "sort-tabs-all-windows";
+
 function retrieve(message) {
   return chrome.i18n.getMessage(message);
 }
@@ -13,29 +16,41 @@ async function initTemplate() {
   <!--<h1> ${retrieve("extensionName")} </h1>
   <br />-->
 
-  <button class="button-primary" href="#" id="sort-tabs-mru"> ${retrieve("buttonSortTabsByMru")} </button>
+  <button class="button-primary" href="#" id="sort-tabs-mru"> ${retrieve(
+    "buttonSortTabsByMru"
+  )} </button>
   <p class="has-text-centered"> (${retrieve("shortcutSortTabsByMru")}) </p>
   <br />
 
-  <button class="button-primary" href="#" id="sort-tabs-favicon-and-title"> ${retrieve("buttonSortTabsByFaviconAndTitle")} </button>
-  <p class="has-text-centered"> (${retrieve("shortcutSortTabsByFaviconAndTitle")}) </p>
+  <button class="button-primary" href="#" id="sort-tabs-favicon-and-title"> ${retrieve(
+    "buttonSortTabsByFaviconAndTitle"
+  )} </button>
+  <p class="has-text-centered"> (${retrieve(
+    "shortcutSortTabsByFaviconAndTitle"
+  )}) </p>
   <br />
 
-  <button class="button-simple" href="#" id="sort-tabs-title"> ${retrieve("buttonSortTabsByTitle")} </button>
+  <button class="button-simple" href="#" id="sort-tabs-title"> ${retrieve(
+    "buttonSortTabsByTitle"
+  )} </button>
   <p> <!--(${retrieve("shortcutSortTabsByTitle")})--> </p>
   <br />
 
-  <button class="button-simple" href="#" id="sort-tabs-url"> ${retrieve("buttonSortTabsByUrl")} </button>
+  <button class="button-simple" href="#" id="sort-tabs-url"> ${retrieve(
+    "buttonSortTabsByUrl"
+  )} </button>
   <p> <!--(${retrieve("shortcutSortTabsByUrl")})--> </p>
   <br />
 
   <label for="sort-tabs-reverse">
-  <input type="checkbox" id="sort-tabs-reverse" ${isReverse ? 'checked' : ''}/>
+  <input type="checkbox" id="sort-tabs-reverse" ${isReverse ? "checked" : ""}/>
    ${retrieve("reverseSorting")} </label>
   <br/>
 
   <label for="sort-tabs-all-windows">
-  <input type="checkbox" id="sort-tabs-all-windows" ${isAllWindows ? 'checked' : ''}/>
+  <input type="checkbox" id="sort-tabs-all-windows" ${
+    isAllWindows ? "checked" : ""
+  }/>
    ${retrieve("allWindows")} </label>
   <br/>
 
@@ -45,15 +60,28 @@ async function initTemplate() {
   </div>
   `;
 
-
   // <input type="checkbox" id="sort-tabs-reverse" ${getReverseToUi() ? 'checked' : ''}/>
 
-  const container = new DOMParser().parseFromString(popupContent, "text/html").getElementById("container");
+  const container = new DOMParser()
+    .parseFromString(popupContent, "text/html")
+    .getElementById("container");
   document.getElementById("container").innerHTML = ``;
   document.getElementById("container").appendChild(container);
+
+  // Restore default options from storage sync
+  document.addEventListener("DOMContentLoaded", restoreOptions);
+
+  // Listeners on checkboxes to persist settings.
+  document
+    .getElementById("container")
+    .querySelectorAll("input[type=checkbox]")
+    .forEach((checkbox) =>
+      checkbox.addEventListener("change", (event) => {
+        event.preventDefault();
+        saveOption(event.target.id, event.target.checked);
+      })
+    );
 }
-
-
 
 // Clicking on the extension "Sort tabs" icon
 document.addEventListener("click", (e) => {
@@ -87,14 +115,46 @@ document.addEventListener("click", (e) => {
       window.close();
       break;
     default:
-      ;
   }
 
   chrome.runtime.sendMessage({
     type: "clickFromPopup",
     command: command,
-    value: value
+    value: value,
   });
 });
+
+// Settings persistence
+function saveOption(key, value) {
+  browser.storage.sync.set({
+    [key]: value,
+  });
+  console.debug("saveOption", key, "=", value);
+}
+
+function restoreOptions() {
+  function setCurrentChoice(id) {
+    return (result) => {
+      if (result[id] === undefined) {
+        console.debug(
+          "restoreOptions.setCurrentChoice: skip restoration because no setting exists yet.",
+          id
+        );
+        return;
+      }
+      document.querySelector(`#${id}`).checked = result[id];
+      console.debug("restoreOptions.setCurrentChoice:", id, result[id]);
+    };
+  }
+
+  onError = (error) => console.log(`Error: ${error}`);
+
+  browser.storage.sync
+    .get(SORT_TABS_REVERSE)
+    .then(setCurrentChoice(SORT_TABS_REVERSE), onError);
+  browser.storage.sync
+    .get(SORT_TABS_ALL_WINDOWS)
+    .then(setCurrentChoice(SORT_TABS_ALL_WINDOWS), onError);
+}
 
 initTemplate();
